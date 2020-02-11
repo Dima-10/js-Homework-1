a282c8c61856413214d8e0ae2811cb41fdfe20ad
dima-10 <dimadragon2x5@gmail.com>
2020-02-11 15:28:39 +0500

    added js

---
+++ jscript.js
@@ -0,0 +1,33 @@
+var num1 = 6;
+var num2 = 3;
+var sum = 9;
+var deduction = 3;
+var multiply = 18;
+var devision = 2;
+var percent = 50;
+
+
+
+var userName = prompt('Введите ваше имя');
+console.log('Меня зовут ' + userName);
+var userAge = prompt('Введите свой возраст');
+console.log( 'Мне ' + userAge);
+var userAnswer1 = prompt('Решите пример 6 + 3');
+console.log( 'Пример 1:' + ' 6 + 3 = ' + sum + ' Ваш ответ ' +  userAnswer1);
+var userAnswer2 = prompt('Решите пример 6 - 3');
+console.log( 'Пример 2:' + ' 6 - 3 = ' + deduction + ' Ваш ответ ' +  userAnswer2);
+var userAnswer3 = prompt('Решите пример 6 * 3');
+console.log( 'Пример 3:' + ' 6 * 3 = ' + multiply + ' Ваш ответ ' +  userAnswer3);
+var userAnswer4 = prompt('Решите пример 6 / 3');
+console.log( 'Пример 4:' + ' 6 / 3 = ' + devision + ' Ваш ответ ' +  userAnswer4);
+var userAnswer5 = prompt('Решите пример 6 % 3');
+console.log( 'Пример 5:' + ' 6 % 3 = ' + percent + ' Ваш ответ ' +  userAnswer5);
+
+
+
+
+
+
+
+
+
